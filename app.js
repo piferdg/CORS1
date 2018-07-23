c9df04defa2cc986849dcbd2fd4fdffcd9167229
Danny Pifer <piferdg@gmail.com>
2018-07-23 15:31:51 -0600

    Addressing port issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const data = require('./dataSet')
-const port = 8000
+const port = parseInt(process.env.PORT || 8000)
 
 const app = express()
 app.use(cors())
